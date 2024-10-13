aec3d995fd268d0d9cee2288d7bd3e5e6632c20f
Shinkuri <shinkuri@kurumi.at>
2024-10-13 02:57:15 +0200

    #1 timestamp prints are now in local time, plus some error handling for the conversion

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,9 @@
-use chrono::{TimeZone, Utc};
+use chrono::{Local, TimeZone, Utc};
+use chrono::LocalResult::Single;
 
 pub fn format_timestamp(timestamp: i64) -> String {
-    Utc.timestamp_opt(timestamp, 0).unwrap().to_rfc3339()
+    match Utc.timestamp_opt(timestamp, 0) {
+        Single(dt) => dt.with_timezone(&Local).to_rfc3339(),
+        _ => format!("Invalid timestamp {}", timestamp)
+    }
 }
